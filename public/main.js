92e441cdfb8a40d2d8be18a446fbaa19b2a0fa38
fernanortega-hub <00191820@uca.edu.sv>
2021-10-28 16:54:25 -0600

    Modifying cards and adding more attributes

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
-let formInfo = document.getElementById('submit-info'); //Obteniendo los elementos del form con id='submit-info'
 let searchInfo = document.getElementById('search'); //Obteniendo los elementos del form con el id='search'
-
+let addInfo = document.getElementById('add'); // Agregando boton que ayudara a guardar los datos
 //Cuando cargue la ventana
 window.onload = () =>{
     renderDataFromSource(getLS('pcWorld')); //Obteniendo los datos de 'pcWorld' desde el localStorage
-    
 }
 
 //Obtner los datos del localStorage
@@ -20,8 +18,8 @@ setLS = (key, value) => {
 //Función que va a mostrar los datos guardados en cards
 renderDataFromSource = (source) => { //Los datos se van a pasar a la función y se van a a guardar en la variable source 
 
-
     let pcCard = document.getElementById('pcWorld-card'); //Obtener los elementos de div
+    let filters = document.getElementById('filters-types'); // Obteniendo propiedades del select
     //Vaciar cards
     pcCard.innerHTML = '';
 
@@ -48,8 +46,17 @@ renderDataFromSource = (source) => { //Los datos se van a pasar a la función y 
             pcPrice.className = "text-center text-sm bg-green-300 m-1 rounded p-0.5 max-h-6";
             pcPrice.innerHTML = '$' + it.price;
 
+            let pcWeight = document.createElement('h3');
+            pcWeight.className = "text-center text-sm bg-green-300 m-1 rounded p-0.5 max-h-6";
+            pcWeight.innerHTML = it.weight+' kg';
+
+            let pcYear = document.createElement('h3');
+            pcYear.className = "text-center text-sm bg-red-400 m-1 rounded p-0.5 max-h-6";
+            pcYear.innerHTML = it.year;
+
             let div = document.createElement('div');//Crear elemento div
             div.className = "w-36 bg-gradient-to-r from-blue-800 via-purple-600 to-indigo-500 flex flex-wrap justify-center py-5 px-3 rounded-xl m-2.5";
+
 
             //Guardando los valores en el div creado
             div.appendChild(pcImg);
@@ -57,6 +64,8 @@ renderDataFromSource = (source) => { //Los datos se van a pasar a la función y 
             div.appendChild(pcAtribute);
             div.appendChild(pcType);
             div.appendChild(pcPrice);
+            div.appendChild(pcWeight);
+            div.appendChild(pcYear);
 
             pcCard.appendChild(div); //Se guarda el div creado anteriormente en el card principal
 
@@ -64,8 +73,8 @@ renderDataFromSource = (source) => { //Los datos se van a pasar a la función y 
     }
 }
  
-//Guardar los elementos del form
-formInfo.onsubmit = (event) => {
+//Guardar los elementos del form por medio de onclick del boton add
+addInfo.onclick = (event) => {
     event.preventDefault(); //Cancela el evento, sinn detener el resto del evento https://developer.mozilla.org/es/docs/Web/API/Event/preventDefault
 
     let product = document.getElementById("product").value;
@@ -73,21 +82,23 @@ formInfo.onsubmit = (event) => {
     let type = document.getElementById('type').value;
     let price = document.getElementById('price').value;
     let img = document.getElementById('img').value;
+    let weight = document.getElementById('weight').value;
+    let year = document.getElementById('year').value;
 
-    let regex = new RegExp('/^\d+$/');
 
     let currLs = getLS('pcWorld'); //Añadiendo key al local storage  
 
-    if (product==""||atribute==""||type==""||(price=="" || isNaN(price))||img=="") // Si hay inputs vacios o hay carecteres no correspondientes, avisar a usuario que rellene todos los datos
+    if (product==""||atribute==""||type==""||(price=="" || isNaN(price))||
+        img==""||(year=="" || isNaN(year) && year < 1500)||(weight=="" || isNaN(weight) || weight < 0)) // Si hay inputs vacios o hay carecteres no correspondientes, avisar a usuario que rellene todos los datos
         return alert('Check data');
 
     let newVal = []; //Declarando arreglo para guardar los elementos
     
     if (!currLs) { //Si el localStorage actual no está vacío
-        newVal.push({product, atribute, type, price, img}); //Entonces pushear los elementos 
+        newVal.push({product, atribute, type, price, img, year, weight}); //Entonces pushear los elementos 
     }
     else{
-        newVal = [...currLs, {product, atribute, type, price, img}]; // Sino asignar los valores
+        newVal = [...currLs, {product, atribute, type, price, img, year, weight}]; // Sino asignar los valores
     }
     
     //Actualizar el localStorage
@@ -102,7 +113,7 @@ searchInfo.onsubmit = (event) =>{
     event.preventDefault();
 
     let searchInp = document.getElementById("searchInp").value; //Otbteniendo la información que introduzca el cliente
-     
+
     //Filtrar por nombre del producto
     let filteredInfo = !(searchInp == "") ? getLS('pcWorld').filter(it => it.product.toLowerCase() == searchInp.toLowerCase()) : getLS('pcWorld');
 
